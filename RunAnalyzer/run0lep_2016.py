689d06a18abf4c1770ed57ef2c93094ca765c7d9
Kyungmin Park <kyungmin.park@cern.ch>
2021-01-11 10:26:40 +0900

    changing out file name

--- RunAnalyzer/run0lep_2016.py
+++ RunAnalyzer/run0lep_2016.py
@@ -15,22 +15,22 @@ python3 run0lep_2016.py /hdfs/store/mc/RunIISummer16NanoAODv6-largeblock/WJetsTo
 
 '''
 
-partDetDir = '$CMSSW_BASE/src/vbfsusy/PartDets/2016/Z_CR1/'
+partDetDir = '$CMSSW_BASE/src/vbfsusy/PartDets/2016/Z_CR2/'
 configDic = {'PartDet':partDetDir+'zjets', 'year': 2016}
 
-#partDetDir = '$CMSSW_BASE/src/vbfsusy/PartDets/2016/W_CR1/'
+#partDetDir = '$CMSSW_BASE/src/vbfsusy/PartDets/2016/W_CR2/'
 #configDic = {'PartDet':partDetDir+'wjets', 'year': 2016}
 
 datafile = sys.argv[1]
 configDic['datafile'] = datafile
-configDic['outname'] = datafile.split('/')[5] + '.root'
+configDic['outname'] = datafile.split('/')[4] + '.root'
 
 # Get PartDets
 if 'data' in datafile:
     if 'B_ver1' in datafile or 'B_ver2' in datafile:  # i.e. SingleMuon_Run2016B.root, not SingleMuon_Run2016B_ver1.root
         configDic['outname'] = 'SingleMuon_Run2016B.root'
     else:
-        configDic['outname'] = datafile.split('/')[5] + '_' + datafile.split('/')[4] + '.root'
+        configDic['outname'] = datafile.split('/')[4] + '_' + datafile.split('/')[3] + '.root'
     configDic['PartDet'] +='_data'
 
 if 'DYJetsToLL_M-10to50_T' in datafile or 'DYJetsToLL_M-50_T' in datafile:
@@ -45,9 +45,9 @@ if not os.path.isdir('Pileup'):
 # Rename the output files for 2017 samples (for the SpecialPUCalculation); the output file name should be one of the branch names from PileUp/new_mc2017_pileupReweighting_NanoAODv6.root
 if configDic['year'] == 2017 and not 'data' in datafile:
     if '_TuneCP5' in datafile:
-        configDic['outname'] = (datafile.split('/')[5]).split('_TuneCP5')[0] + '_TuneCP5.root'
+        configDic['outname'] = (datafile.split('/')[4]).split('_TuneCP5')[0] + '_TuneCP5.root'
     elif '_13TeV' in datafile:
-        configDic['outname'] = (datafile.split('/')[5]).split('_13TeV')[0] + '_13TeV.root'
+        configDic['outname'] = (datafile.split('/')[4]).split('_13TeV')[0] + '_13TeV.root'
     
 runcode = '$CMSSW_BASE/src/NanoAOD_Analyzer/Analyzer -out {outname} -y {year} -C {PartDet} -in {datafile} '.format(**configDic)
 #runcode = '$CMSSW_BASE/src/Forked/NanoAOD_Analyzer/Analyzer -out {outname} -y {year} -C {PartDet} -in {datafile} '.format(**configDic)
--- RunAnalyzer/run0lep_2018.py
+++ RunAnalyzer/run0lep_2018.py
@@ -23,14 +23,14 @@ configDic = {'PartDet':partDetDir+'zjets', 'year': 2018}
 
 datafile = sys.argv[1]
 configDic['datafile'] = datafile
-configDic['outname'] = datafile.split('/')[5] + '.root'
+configDic['outname'] = datafile.split('/')[4] + '.root'
 
 # Get PartDets
 if 'data' in datafile:
     if 'B_ver1' in datafile or 'B_ver2' in datafile:  # i.e. SingleMuon_Run2016B.root, not SingleMuon_Run2016B_ver1.root
         configDic['outname'] = 'SingleMuon_Run2016B.root'
     else:
-        configDic['outname'] = datafile.split('/')[5] + '_' + datafile.split('/')[4] + '.root'
+        configDic['outname'] = datafile.split('/')[4] + '_' + datafile.split('/')[3] + '.root'
     configDic['PartDet'] +='_data'
 
 if 'DYJetsToLL_M-10to50_T' in datafile or 'DYJetsToLL_M-50_T' in datafile:
@@ -45,9 +45,9 @@ if not os.path.isdir('Pileup'):
 # Rename the output files for 2017 samples (for the SpecialPUCalculation); the output file name should be one of the branch names from PileUp/new_mc2017_pileupReweighting_NanoAODv6.root
 if configDic['year'] == 2017 and not 'data' in datafile:
     if '_TuneCP5' in datafile:
-        configDic['outname'] = (datafile.split('/')[5]).split('_TuneCP5')[0] + '_TuneCP5.root'
+        configDic['outname'] = (datafile.split('/')[4]).split('_TuneCP5')[0] + '_TuneCP5.root'
     elif '_13TeV' in datafile:
-        configDic['outname'] = (datafile.split('/')[5]).split('_13TeV')[0] + '_13TeV.root'
+        configDic['outname'] = (datafile.split('/')[4]).split('_13TeV')[0] + '_13TeV.root'
     
 runcode = '$CMSSW_BASE/src/NanoAOD_Analyzer/Analyzer -out {outname} -y {year} -C {PartDet} -in {datafile} '.format(**configDic)
 #runcode = '$CMSSW_BASE/src/Forked/NanoAOD_Analyzer/Analyzer -out {outname} -y {year} -C {PartDet} -in {datafile} '.format(**configDic)
